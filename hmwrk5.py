e257be2a19d6cb48a3326f380419ff985b257fa9
Taxupka <167883032+Taxupka@users.noreply.github.com>
2024-05-05 10:25:34 +0300

    Add files via upload

---
+++ hmwrk5.py
@@ -0,0 +1,16 @@
+my_list = ['apple','apricot','banana','peach','orange','lemon']
+print(my_list)
+a = my_list[0]
+b = my_list[-1]
+print(a,b)
+print(my_list[2:5])
+my_list[2]='plum'
+print(my_list)
+my_dict = {'meat':'мясо','eat':'еда','people':'люди','Earth':'земля','child':'ребенок'}
+print(my_dict)
+print(my_dict.get('people'))
+my_dict['people']=567
+my_dict['power']='сила'
+print(my_dict)
+
+
